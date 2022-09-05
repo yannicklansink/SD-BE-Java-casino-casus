f25a0d1364de8af6ed9a6d50cced3e7efe3818fb
Yannick Lansink <lansy00@macos-c02gd6j8q6l4-lansy00.wifi.belastingdienst.nl>
2022-09-05 09:59:24 +0200

    added higherlower option

--- start/novi/Main.java
+++ start/novi/Main.java
@@ -7,6 +7,20 @@ import novi.higherlower.HigherLowerGame;
 public class Main {
 
     public static void main(String[] args) {
+
+        /*
+        De speler start het spel (je start de applicatie door het starten van de ‘main’ methode)
+        Het spel kiest een willekeurig nummer tussen 0 en 100 dat geraden moet worden.
+        Het spel zet het aantal gespeelde beurten op 0
+        Het spel print de regel ‘Make a guess’
+        De speler raadt een nummer
+        Het spel hoogt het aantal gespeelde beurten op met 1
+        Was het nummer te hoog? Het spel print de regel `That number is too high!` (terug naar stap 4)
+        Was het nummer te laag? Het spel print de regel `That number is too low!` (terug naar stap 4)
+        Komt het nummer overeen? Het spel print de regel `Correct! You guessed the number in x turns` waarbij x het aantal gespeelde beurten is. (het spel wordt gestopt)
+
+
+         */
         Scanner inputScanner = new Scanner(System.in);
         HigherLowerGame game = new HigherLowerGame(inputScanner);
 
--- start/novi/higherlower/HigherLowerGame.java
+++ start/novi/higherlower/HigherLowerGame.java
@@ -5,23 +5,41 @@ import java.util.Scanner;
 public class HigherLowerGame {
     private Scanner inputScanner;
     private boolean gameIsRunning;
+    private int guesses;
+    private int randomInt;
+    private final static int MIN = 1;
+    private final static int MAX = 100;
 
     public HigherLowerGame(Scanner inputScanner) {
         this.inputScanner = inputScanner;
+        guesses = 0;
+        randomInt = MIN + (int)(Math.random() * ((MAX - MIN) + 1));
+        System.out.println("CHEATMODE to be guessed: " + randomInt);
     }
 
     public void playGame() {
         gameIsRunning = true;
-
         // Here you should generate the number to guess
 
         while (gameIsRunning) {
             System.out.println("Make a guess");
 
-            var input = inputScanner.nextLine();
+            int input = inputScanner.nextInt();
             System.out.printf("You typed: %s\n", input);
 
             // Check if the guess is correct, if so end the game
+            checkGuessesNumber(input);
+        }
+    }
+
+    private void checkGuessesNumber(int input) {
+        if (input > randomInt) {
+            System.out.println("You guessed to high!");
+        } else if (input < randomInt) {
+            System.out.println("You guessed to low");
+        } else {
+            System.out.println("You guessed correct!");
+            gameIsRunning = false;
         }
     }
 }
